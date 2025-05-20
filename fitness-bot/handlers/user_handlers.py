ca844d1ed1df65bc33a279c9aaf35a5b7c5c2bc1
sudrain <densedra@gmail.com>
2025-05-20 18:31:07 +0500

    add states handlers

--- fitness-bot/handlers/user_handlers.py
+++ fitness-bot/handlers/user_handlers.py
@@ -13,18 +13,3 @@ async def get_id(message: types.Message):
 @user_router.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-
-
-@user_router.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
--- fitness-bot/handlers/workout_handlers.py
+++ fitness-bot/handlers/workout_handlers.py
@@ -1,15 +1,79 @@
-from aiogram import Router
+from aiogram import F, Router, types
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from states import WorkoutForm
 
 workout_router = Router()
 
 
 @workout_router.message(Command("add_workout"))
-async def command_add_workout(message: Message) -> None:
-    await message.answer("workout")
+async def start_workout_registration(message: types.Message, state: FSMContext):
+    await message.answer(
+        "Выберите тип тренировки:",
+        reply_markup=types.ReplyKeyboardMarkup(
+            keyboard=[
+                [types.KeyboardButton(text="Силовая")],
+                [types.KeyboardButton(text="Кардио")],
+            ],
+            resize_keyboard=True,
+        ),
+    )
+    await state.set_state(WorkoutForm.select_workout_type)
 
 
-@workout_router.message(Command("progress"))
-async def command_progress(message: Message) -> None:
-    await message.answer("progress")
+# Обработка типа тренировки
+@workout_router.message(WorkoutForm.select_workout_type)
+async def process_workout_type(message: types.Message, state: FSMContext):
+    await state.update_data(workout_type=message.text)
+    await message.answer(
+        "Введите продолжительность(в минутах):", reply_markup=types.ReplyKeyboardRemove()
+    )
+    await state.set_state(WorkoutForm.enter_duration)
+
+
+# Обработка продолжительности
+@workout_router.message(WorkoutForm.enter_duration)
+async def procces_duration(message: types.Message, state: FSMContext):
+    if not message.text.isdigit():
+        await message.answer("Введите число!")
+        return
+
+    await state.update_data(duration=int(message.text))
+    await message.answer("Введите дату тренировки (ДД.ММ.ГГГГ):")
+    await state.set_state(WorkoutForm.enter_date)
+
+
+# Обработка Даты и подтверждение
+@workout_router.message(WorkoutForm.enter_date)
+async def process_date(message: types.Message, state: FSMContext):
+    """
+    Тут нужно добавить валидацию даты либо добавить автодату в sql
+    """
+    data = await state.get_data()
+    await message.answer(
+        f"Проверьте данные:\n"
+        f"Тип: {data['workout_type']}\n"
+        f"Длительность: {data['duration']} мин\n"
+        f"Дата: {message.text}",
+        reply_markup=types.InlineKeyboardMarkup(
+            inline_keyboard=[
+                [types.InlineKeyboardButton(text="Подтвердить", callback_data="confirm")]
+            ]
+        ),
+    )
+    await state.update_data(date=message.text)
+    await state.set_state(WorkoutForm.confirm_data)
+
+
+# Сохранение данных
+@workout_router.callback_query(F.data == "confirm", WorkoutForm.confirm_data)
+async def save_workout(callback: types.CallbackQuery, state: FSMContext):
+    # Добавим БД позже
+    # data = await state.get_data()
+    await callback.message.answer("✅ Данные сохранены!")
+    await state.clear()
+
+
+# @workout_router.message(Command("progress"))
+# async def command_progress(message: types.Message) -> None:
+#     await message.answer("progress")
